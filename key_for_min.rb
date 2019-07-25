d576cfd64746faa950271eabdfa6613a77d26662
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-25 00:58:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
       val = 0 
       lowest = nil 
-      name_hash.collect |name, value | 
+      name_hash.collect |name, value| 
       
       if val == 0 
         val = value 
